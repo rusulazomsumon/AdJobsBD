0e1a8d35f0b8e6534eb5b27fbd4f99d46ca679f0
sumon <18103020@iubat.edu>
2023-09-19 20:41:29 +0600

    homepage template parts added

---
+++ resources/views/frontend/template/carousel.blade.php
@@ -0,0 +1,38 @@
+<div class="container-fluid p-0">
+            <div class="owl-carousel header-carousel position-relative">
+                <!-- slider -->
+                @foreach($sliders as $slider)
+                <div class="owl-carousel-item position-relative">
+                    <img class="img-fluid" src="{{ asset('assets/img/carousel-1.jpg') }}" alt="">
+                    <!-- <img class="img-fluid" src="{{ $slider->bg_img_url }}" width="100%" height="800px" alt=""> -->
+                    <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(43, 57, 64, .5);">
+                        <div class="container">
+                            <div class="row justify-content-start">
+                                <div class="col-10 col-lg-8">
+                                    <h1 class="display-3 text-white animated slideInDown mb-4">{{ $slider->title}}</h1>
+                                    <p class="fs-5 fw-medium text-white mb-4 pb-2">{{ $slider->des}}</p>
+                                    <a href="{{ $slider->btn1_url}}" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">{{ $slider->btn1_txt}}</a>
+                                    <a href="{{ $slider->btn2_url}}" class="btn btn-secondary py-md-3 px-md-5 animated slideInRight">{{ $slider->btn2_txt}}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                <!-- <div class="owl-carousel-item position-relative">
+                    <img class="img-fluid" src="{{ asset('assets/img/carousel-2.jpg') }}" alt="">
+                    <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(43, 57, 64, .5);">
+                        <div class="container">
+                            <div class="row justify-content-start">
+                                <div class="col-10 col-lg-8">
+                                    <h1 class="display-3 text-white animated slideInDown mb-4">Find The Best Startup Job That Fit You</h1>
+                                    <p class="fs-5 fw-medium text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                    <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Search A Job</a>
+                                    <a href="" class="btn btn-secondary py-md-3 px-md-5 animated slideInRight">Find A Talent</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </div>
--- resources/views/frontend/template/cvgenarator.blade.php
+++ resources/views/frontend/template/cvgenarator.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="container-xxl bg-light py-5">
+    <div class="container">
+        <h1 class="text-center wow fadeInUp" data-wow-delay="0.1s">CV Generator</h1>
+        <h3 class="text-center " data-wow-delay="0.2s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, asperiores.</h3>
+        <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
+            <a class="btn btn-primary py-3 px-5" href="#">Create Now</a>
+        </div>
+    </div>
+</div>
