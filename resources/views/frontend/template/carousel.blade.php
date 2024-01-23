f7bbe20447cc73a027ac7410d85cc4b647ace9d5
RusulAzomSumon <18103020@iubat.edu>
2024-01-23 23:53:55 +0600

    GI U

--- resources/views/frontend/template/carousel.blade.php
+++ resources/views/frontend/template/carousel.blade.php
@@ -25,8 +25,8 @@
                         <div class="container">
                             <div class="row justify-content-start">
                                 <div class="col-10 col-lg-8">
-                                    <h1 class="display-3 text-white animated slideInDown mb-4">Find The Best Startup Job That Fit You</h1>
-                                    <p class="fs-5 fw-medium text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                    <h1 class="display-3 text-white animated slideInDown mb-4">Bangladesh 1st Eclusive Job Portals</h1>
+                                    <p class="fs-5 fw-medium text-white mb-4 pb-2">Disability is a different Kinds of Ability! </p>
                                     <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Search A Job</a>
                                     <a href="" class="btn btn-secondary py-md-3 px-md-5 animated slideInRight">Find A Talent</a>
                                 </div>
